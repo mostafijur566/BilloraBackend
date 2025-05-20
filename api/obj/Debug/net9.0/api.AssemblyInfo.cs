126d9ad91fce3c4f92c9f77d6df3ea68701e589f
Mostafijur Rahman <mostafijur1812@gmail.com>
2025-05-20 17:08:37 +0600

    Save current local changes before pulling

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f16c5961d406a8d54cf0b6c3058e97d82538288")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26fbce7422e53ad0ea9094c90f58eba73f32595")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
