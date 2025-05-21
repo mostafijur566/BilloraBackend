d27da92497352e109d6c8f69bb3408630e36249e
Mostafijur Rahman <mostafijur1812@gmail.com>
2025-05-21 18:40:24 +0600

    Customer model added

---
+++ api/Models/Customer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string ContactPerson { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        
+        // Navigation property
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+    }
+}
